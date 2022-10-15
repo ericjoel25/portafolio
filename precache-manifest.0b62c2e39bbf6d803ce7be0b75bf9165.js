bac243d9fe8de3ee7a73ca9cb7f1cd41828c5c4e
Eric Joel <ericjoelmath@gmail.com>
2022-10-14 20:35:58 -0400

    Updates

---
+++ precache-manifest.0b62c2e39bbf6d803ce7be0b75bf9165.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = [
+  {
+    "revision": "8a7ced6215954e29aa95",
+    "url": "/ericjoel25.github.io/portafolio/static/css/main.109fe426.chunk.css"
+  },
+  {
+    "revision": "8a7ced6215954e29aa95",
+    "url": "/ericjoel25.github.io/portafolio/static/js/main.59e0c5a6.chunk.js"
+  },
+  {
+    "revision": "fa19c80fdaaab257f5a4",
+    "url": "/ericjoel25.github.io/portafolio/static/js/runtime~main.0e256c63.js"
+  },
+  {
+    "revision": "690c3958477dd65b256a",
+    "url": "/ericjoel25.github.io/portafolio/static/js/2.e06082f5.chunk.js"
+  },
+  {
+    "revision": "1ad0af76033d25b423aa",
+    "url": "/ericjoel25.github.io/portafolio/static/js/3.11315e87.chunk.js"
+  },
+  {
+    "revision": "be9aab84e294d31ca0bdf0a28d266426",
+    "url": "/ericjoel25.github.io/portafolio/static/media/Eric2.be9aab84.png"
+  },
+  {
+    "revision": "6f90543646ff449152fbd2adba129226",
+    "url": "/ericjoel25.github.io/portafolio/static/media/homeImg.6f905436.png"
+  },
+  {
+    "revision": "9ad79828751ede5dbe9f92f97237bdf2",
+    "url": "/ericjoel25.github.io/portafolio/static/media/Firebase.9ad79828.png"
+  },
+  {
+    "revision": "666c190e1f7a462852f10506d3375b5d",
+    "url": "/ericjoel25.github.io/portafolio/static/media/HTML.666c190e.png"
+  },
+  {
+    "revision": "10550ee86b47ea33b21c4df8c6a053e8",
+    "url": "/ericjoel25.github.io/portafolio/static/media/CSS.10550ee8.png"
+  },
+  {
+    "revision": "e0cc0801844dee4565ba5610244d98c3",
+    "url": "/ericjoel25.github.io/portafolio/static/media/Javascript.e0cc0801.png"
+  },
+  {
+    "revision": "0d590b16bf79ddd8d2a34aba700d3e92",
+    "url": "/ericjoel25.github.io/portafolio/static/media/React.0d590b16.png"
+  },
+  {
+    "revision": "04557c1b0b790482dafb1bf4e082b69e",
+    "url": "/ericjoel25.github.io/portafolio/static/media/ReactNative.04557c1b.png"
+  },
+  {
+    "revision": "5ba64b1e89af4233ff954d1f449b2795",
+    "url": "/ericjoel25.github.io/portafolio/static/media/Tauri.5ba64b1e.png"
+  },
+  {
+    "revision": "251b2b009fb1a5f68c8dc0ad11448854",
+    "url": "/ericjoel25.github.io/portafolio/static/media/Granja.251b2b00.png"
+  },
+  {
+    "revision": "43f091e2d1e138073d1a19c680368938",
+    "url": "/ericjoel25.github.io/portafolio/static/media/venta.43f091e2.png"
+  },
+  {
+    "revision": "bd0c0ca8f1b5a929c53764b9e26af641",
+    "url": "/ericjoel25.github.io/portafolio/static/media/Ortografia.bd0c0ca8.jpg"
+  },
+  {
+    "revision": "c9cc766d1ba9551e42b2c4181f6adb1a",
+    "url": "/ericjoel25.github.io/portafolio/index.html"
+  }
+];
